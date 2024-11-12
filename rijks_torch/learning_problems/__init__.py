b2393bee9357a364b168f2e2a39b1c4210a6d54c
Sean Cheng <seanacheng@gmail.com>
2024-11-11 19:03:54 -0500

    remove tiny vit model

--- rijks_torch/learning_problems/__init__.py
+++ rijks_torch/learning_problems/__init__.py
@@ -2,7 +2,6 @@
 
 from .vit import get_vit_b_16_problem
 from .vit import get_vit_b_16_drop_problem
-from .vit import get_vit_t_16_problem
 from .swin import get_swin_b_problem
 from .swin import get_swin_b_drop_problem
 from .swin import get_swin_s_problem
