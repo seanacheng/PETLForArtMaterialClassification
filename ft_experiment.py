922a5b48e64b5fb02dd7c2911fe2b0deddc82ec7
Sean Cheng <seanacheng@gmail.com>
2024-11-19 14:59:21 -0500

    incorporate both architectures in hyperparameter search over training

--- ft_experiment.py
+++ ft_experiment.py
@@ -15,9 +15,6 @@ def main():
     # Creating the dataloaders from given arguments:
     train_loader, val_loader, test_loader = RijksDataloader.make_data_loaders(batch_size=128, transform=defs.buildTransform(imnet_norm=True))
 
-    # Get the model tailored to specification. Using getattr because function from cli args
-    model = ViTModel(method="ft")
-
     seeds = [17, 204, 596]
     lrs = [0.001, 0.01, 0.1]
     l2pen = 0.1
@@ -26,35 +23,44 @@ def main():
 
     figures = []
     
-    for lr in lrs:
-        for seed in seeds:
-            model, results = train(model, train_loader, val_loader, lr, epochs, seed, l2pen)
-            # Testing model that performed best on validation set:
-            final_acc = test(model, test_loader) 
-            print(f"final accuracy: {final_acc}")
-            
-            temp_df = pd.DataFrame({
-                'total_loss': results['tr']['loss'],
-                'train_loss': results['tr']['xent'],
-                'train_err':  results['tr']['err'],
-                'val_loss':   results['va']['xent'],
-                'val_err':    results['va']['err'],
-                'epoch':      results['epochs']
-            })
-            fig, ax = plt.subplots(figsize=(9,4))
-            ax.plot(results['epochs'], results['tr']['loss'], '--', color='b', label='tr loss')
-            ax.plot(results['epochs'], results['tr']['err'], '-', color='b', label='tr err')
+    for model in ["vit", "swin"]:
+        for lr in lrs:
+            for seed in seeds:
+                # Generate a new pretrained model every time
+                pretrained_model = None
+                if model == "vit":
+                    pretrained_model = ViTModel(method="ft")
+                elif model == "swin":
+                    pretrained_model = SwinModel(method="ft")
+                trained_model, results = train(pretrained_model, train_loader, val_loader, lr, epochs, seed, l2pen)
 
-            ax.plot(results['epochs'], results['va']['xent'], '--', color='r', label='va xent')
-            ax.plot(results['epochs'], results['va']['err'], '-', color='r', label='va err')
-            ax.set_title(f'ViT FT test accuracy={final_acc}\nlr={lr}, seed={seed}')
-            ax.legend()
-            figures.append(fig)
-            fig.savefig(f'results/vit_ft_lr_{lr}_seed_{seed}.png')
+                # Testing model that performed best on validation set:
+                final_acc = test(trained_model, test_loader) 
+                print(f"final accuracy: {final_acc}")
+                
+                temp_df = pd.DataFrame({
+                    'total_loss': results['tr']['loss'],
+                    'train_loss': results['tr']['xent'],
+                    'train_err':  results['tr']['err'],
+                    'val_loss':   results['va']['xent'],
+                    'val_err':    results['va']['err'],
+                    'epoch':      results['epochs']
+                })
+                fig, ax = plt.subplots(figsize=(9,4))
+                ax.plot(results['epochs'], results['tr']['loss'], '--', color='b', label='tr loss')
+                ax.plot(results['epochs'], results['tr']['err'], '-', color='b', label='tr err')
 
-            df = pd.concat([df, temp_df], ignore_index=True)
+                ax.plot(results['epochs'], results['va']['xent'], '--', color='r', label='va xent')
+                ax.plot(results['epochs'], results['va']['err'], '-', color='r', label='va err')
+                ax.set_title(f'ViT FT test accuracy={final_acc}\nlr={lr}, seed={seed}')
+                ax.legend()
+                figures.append(fig)
+                fig.savefig(f'results/{model}_ft_lr_{lr}_seed_{seed}.png')
 
-    df.to_csv(f'results/ViT_ft_results.csv')
+                df = pd.concat([df, temp_df], ignore_index=True)
+
+        df.to_csv(f'results/{model}_ft_results.csv')
+
     for fig in figures:
         fig.show()
 
