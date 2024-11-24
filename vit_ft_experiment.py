6ffacca849a23fc5a8599655596d492a8a42f0c3
Sean Cheng <seanacheng@gmail.com>
2024-11-24 12:52:37 -0500

    save plots for all experiments

--- vit_ft_experiment.py
+++ vit_ft_experiment.py
@@ -15,44 +15,55 @@ def main():
     # Creating the dataloaders from given arguments:
     train_loader, val_loader, test_loader = RijksDataloader.make_data_loaders(batch_size=128, transform=defs.buildTransform(imnet_norm=True))
 
-    seeds = [17, 596, 2043]
     lrs = [0.001, 0.01, 0.1]
+    seeds = [17, 596, 2043]
     l2pens = [0.0, 0.01, 0.1]
+    seed = 596  # simplify training 
+    l2pen = 0.01  # simplify training
     epochs = 100
 
+    first_run = True
     best_acc = 0
     for lr in lrs:
-        for seed in seeds:
-            for l2pen in l2pens:
-                pretrained_model = ViTModel(method="ft")
-                # Training and validating (best model on val set returned):
-                trained_model, results = train(pretrained_model, train_loader, val_loader, lr, epochs, seed, l2pen)
+        # for seed in seeds:
+        #     for l2pen in l2pens:
+        pretrained_model = ViTModel(method="ft")
+        # Training and validating (best model on val set returned):
+        trained_model, results = train(pretrained_model, train_loader, val_loader, lr, epochs, seed, l2pen)
 
-                # Testing model that performed best on validation set:
-                accuracy, balanced_acc = test(trained_model, test_loader) 
-                print("final accuracy: {}, balanced accuracy: {}".format(accuracy, balanced_acc))
-                if balanced_acc > best_acc:
-                    best_acc = balanced_acc
-                    torch.save(trained_model.state_dict(), "results/best_ViT_FT_model.pth")
+        # Testing model that performed best on validation set:
+        accuracy, balanced_acc = test(trained_model, test_loader) 
+        print("final accuracy: {}, balanced accuracy: {}".format(accuracy, balanced_acc))
+        if balanced_acc > best_acc:
+            best_acc = balanced_acc
+            torch.save(trained_model.state_dict(), "results/best_ViT_FT_model.pth")
 
-                df = pd.DataFrame({
-                    'total_loss': results['tr']['loss'],
-                    'train_loss': results['tr']['xent'],
-                    'train_err':  results['tr']['err'],
-                    'val_loss':   results['va']['xent'],
-                    'val_err':    results['va']['err'],
-                    'epoch':      results['epochs']
-                })
+        df = pd.DataFrame({
+            'total_loss': results['tr']['loss'],
+            'train_loss': results['tr']['xent'],
+            'train_err':  results['tr']['err'],
+            'val_loss':   results['va']['xent'],
+            'val_err':    results['va']['err'],
+            'epoch':      results['epochs'],
+            'seed':       results['seed'],
+            'lr':         results['lr'],
+            'l2pen':      results['l2pen']
+        })
+        if first_run:
+            df.to_csv('results/ViT_FT.csv', mode="w", index=False, header=True) # overwrites if file already exists
+            first_run = False
+        else:
+            df.to_csv('results/ViT_FT.csv', mode="a", index=False, header=False)
 
-                plt.plot(results['epochs'], results['tr']['loss'], '--', color='b', label='tr loss')
-                plt.plot(results['epochs'], results['tr']['err'], '-', color='b', label='tr err')
+        plt.plot(results['epochs'], results['tr']['loss'], '--', color='b', label='tr loss')
+        plt.plot(results['epochs'], results['tr']['err'], '-', color='b', label='tr err')
 
-                plt.plot(results['epochs'], results['va']['xent'], '--', color='r', label='va xent')
-                plt.plot(results['epochs'], results['va']['err'], '-', color='r', label='va err')
-                plt.title('ViT FT\nlr={},seed={},l2pen={}'.format(lr, seed, l2pen))
-                plt.legend()
+        plt.plot(results['epochs'], results['va']['xent'], '--', color='r', label='va xent')
+        plt.plot(results['epochs'], results['va']['err'], '-', color='r', label='va err')
+        plt.title('ViT FT\nlr={},seed={},l2pen={}'.format(lr, seed, l2pen))
+        plt.legend()
+        plt.savefig('results/ViT_FT_lr{}_seed{}_l2pen{}.jpg'.format(str(lr)[2:], seed, str(l2pen)[2:]))
 
-                df.to_csv('results/ViT_FT_lr{}_seed{}_l2pen{}.csv'.format(str(lr)[2:], seed, str(l2pen)[2:]))
 
 
 if __name__ == "__main__":
