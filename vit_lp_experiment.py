8fadbd422da37c38939d101ac0a27388a2c29b24
Sean Cheng <seanacheng@gmail.com>
2024-12-01 17:05:53 -0500

    add 2 more seeds for calculating accuracy over multiple runs

--- vit_lp_experiment.py
+++ vit_lp_experiment.py
@@ -15,7 +15,7 @@ def main():
     train_loader, val_loader, test_loader = RijksDataloader.make_data_loaders(batch_size=128, transform=defs.buildTransform(imnet_norm=True))
 
     lrs = [0.001, 0.01]
-    seeds = [17, 596, 2043]
+    seeds = [17, 381, 596, 1365, 2043]
     l2pen = 0.01  # simplify training
     epochs = 100
 
