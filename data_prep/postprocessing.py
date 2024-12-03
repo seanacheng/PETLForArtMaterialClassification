6a7304f7aab94a3345c91a911a3fefa2cfbdfe46
Sean Cheng <seanacheng@gmail.com>
2024-12-02 22:15:27 -0500

    redesign plot for accuracy vs lr

--- data_prep/postprocessing.py
+++ data_prep/postprocessing.py
@@ -3,20 +3,18 @@ import matplotlib.pyplot as plt
 
 def create_visualization(arch, method):
     # Load the CSV file
-    df = pd.read_csv('results/{}_{}.csv'.format(arch, method))
-    # Group by epoch and compute mean and standard deviation
-    grouped = df.groupby('epoch').agg(['mean', 'std'])
+    df = pd.read_csv('results/{}_{}_hyperparameter_search.csv'.format(arch, method))
+    # Group by lr and compute mean and standard deviation
+    grouped = df.groupby('lr').agg(['mean', 'std'])
     # Extract epochs
     epochs = grouped.index
 
-    plot_with_std(epochs, grouped['total_loss']['mean'], grouped['total_loss']['std'], '--', 'b', 'Total Loss')
-    plot_with_std(epochs, grouped['train_err']['mean'], grouped['train_err']['std'], '-', 'b', 'Train Error')
-    plot_with_std(epochs, grouped['val_loss']['mean'], grouped['val_loss']['std'], '--', 'r', 'Validation Loss')
-    plot_with_std(epochs, grouped['val_err']['mean'], grouped['val_err']['std'], '-', 'r', 'Validation Error')
-
+    plot_with_std(epochs, grouped['balanced_acc']['mean'], grouped['balanced_acc']['std'], '-', 'b', 'l2pen=0.01')
+    
     # Add labels and legend
-    plt.xlabel('Epoch')
-    plt.ylabel('Value')
+    plt.xscale('log')
+    plt.xlabel('Learning Rate')
+    plt.ylabel('Balanced Accuracy')
     plt.legend()
     plt.title('{} {} Mean and Standard Deviation'.format(arch, method))
     plt.savefig('results/{}_{}.jpg'.format(arch, method))
@@ -27,9 +25,9 @@ def plot_with_std(x, y_mean, y_std, linestyle, color, label):
     plt.plot(x, y_mean, linestyle=linestyle, color=color, label=label)
     plt.fill_between(x, y_mean - y_std, y_mean + y_std, alpha=0.2)
 
-create_visualization("ViT", "LP")
-create_visualization("ViT", "FT")
-create_visualization("ViT", "ST")
-create_visualization("Swin", "LP")
-create_visualization("Swin", "FT")
-create_visualization("Swin", "ST")
+# create_visualization("ViT", "LP")
+# create_visualization("ViT", "FT")
+# create_visualization("ViT", "ST")
+# create_visualization("Swin", "LP")
+# create_visualization("Swin", "FT")
+# create_visualization("Swin", "ST")
