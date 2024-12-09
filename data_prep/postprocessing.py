1d44d40831de7ddff673a225660ad55e2d488cc4
Sean Cheng <seanacheng@gmail.com>
2024-12-08 22:03:27 -0500

    plot loss functions

--- data_prep/postprocessing.py
+++ data_prep/postprocessing.py
@@ -2,6 +2,38 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def create_loss_visualization_subplot(ax, arch, method):
+    df = pd.read_csv(f'results/{arch}_{method}.csv')
+    grouped = df.groupby('epoch').agg(['mean', 'std'])
+    epochs = grouped.index
+
+    plot_with_std(ax, epochs, grouped['total_loss']['mean'], grouped['total_loss']['std'], '--', 'b', 'Total Loss')
+    plot_with_std(ax, epochs, grouped['train_err']['mean'], grouped['train_err']['std'], '-', 'b', 'Train Error')
+    plot_with_std(ax, epochs, grouped['val_loss']['mean'], grouped['val_loss']['std'], '--', 'r', 'Validation Loss')
+    plot_with_std(ax, epochs, grouped['val_err']['mean'], grouped['val_err']['std'], '-', 'r', 'Validation Error')
+    ax.set_xlabel('Epoch')
+    ax.set_ylabel('Value')
+    ax.legend()
+    ax.set_title(f'{arch} {method} Loss Per Epoch')
+
+def plot_with_std(ax, x, y_mean, y_std, linestyle, color, label):
+    ax.plot(x, y_mean, linestyle=linestyle, color=color, label=label)
+    ax.fill_between(x, y_mean - y_std, y_mean + y_std, color=color, alpha=0.2)
+
+def create_loss_plots(arch):
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6), sharey=True)
+
+    create_loss_visualization_subplot(axes[0], arch, "LP")
+    create_loss_visualization_subplot(axes[1], arch, "FT")
+    create_loss_visualization_subplot(axes[2], arch, "ST")
+
+    plt.tight_layout()
+    plt.savefig('results/{}_loss_per_epoch.jpg'.format(arch))
+
+create_loss_plots("ViT")
+create_loss_plots("Swin")
+
 def create_bal_acc_visualization(arch):
     lp_df = pd.read_csv('results/{}_LP_hyperparameter_search.csv'.format(arch))
     ft_df = pd.read_csv('results/{}_FT_hyperparameter_search.csv'.format(arch))
@@ -26,7 +58,6 @@ def create_bal_acc_visualization(arch):
     plt.show()
 
 
-# Plotting function
 def plot_with_std(x, y_mean, y_std, linestyle, color, label, fill_color):
     plt.plot(x, y_mean, linestyle=linestyle, color=color, label=label)
     plt.fill_between(x, y_mean - y_std, y_mean + y_std, color=fill_color, alpha=0.2)
